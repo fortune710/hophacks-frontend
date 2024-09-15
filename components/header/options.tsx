13edfe2823d9340cb348f171affe75e679107a0a
fortune710 <fortune0208@yahoo.com>
2024-09-15 06:18:06 -0400

    bug: fixed failing next builds

--- components/header/options.tsx
+++ components/header/options.tsx
@@ -12,7 +12,7 @@ export default function Options() {
             href="/home"
             className={cn(
               "transition-colors hover:text-foreground",
-              pathname.includes("/home")
+              pathname?.includes("/home")
                 ? "text-foreground"
                 : "text-muted-foreground"
             )}
@@ -20,26 +20,6 @@ export default function Options() {
             Home
           </Link>
 
-        <Link
-          href="/predictions"
-          className={cn(
-            "transition-colors hover:text-foreground",
-            pathname === "/predictions" ? "text-foreground" : "text-muted-foreground"
-          )}
-        >
-          
-        </Link>
-        <Link
-          href="/maps"
-          className={cn(
-            "transition-colors hover:text-foreground",
-            pathname.includes("/maps")
-              ? "text-foreground"
-              : "text-muted-foreground"
-          )}
-        >
-          
-        </Link>
         </>
     )
 }
